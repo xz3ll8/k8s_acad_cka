b6368fbd5bc54dff0cdfc5493e6c38f49ddf758a
xz3ll8 <nicholasost7@gmail.com>
2018-12-08 17:17:29 +0000

    K8s Centos Setup

--- k8s_centos_install.sh
+++ k8s_centos_install.sh
@@ -41,13 +41,4 @@ EOF
 
 sudo sysctl --system
 
-# Initialize the Kube Master. Do this only on the master node.
-sudo kubeadm init --pod-network-cidr=10.244.0.0/16 --ignore-preflight-checks "NumCPU"
-mkdir -p $HOME/.kube
-sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-sudo chown $(id -u):$(id -g) $HOME/.kube/config
-
-# Install flannel networking.
-kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/bc79dd1505b0c8681ece4de4c0d86c5cd2643275/Documentation/kube-flannel.yml
-
-exit
+exit;
