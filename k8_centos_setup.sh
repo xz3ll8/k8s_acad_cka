709d41fb3697e91b0dcd5ee34dc823ed62277d49
njodev <nicholasost7@gmail.com>
2019-02-19 09:12:58 +0000

    Correct NUMCPU preflight flag k8s_centos_install.sh

--- k8_centos_setup.sh
+++ k8_centos_setup.sh
@@ -1,12 +1,17 @@
 # K8s Centos Setup
 
 # Initialize the Kube Master. Do this only on the master node.
-sudo kubeadm init --pod-network-cidr=10.244.0.0/16 --ignore-preflight-checks "NumCPU"
+# Changed for Calico Network Plugin
+
+sudo kubeadm init --pod-network-cidr=192.168.0.0/16 --ignore-preflight-check=NumCPU
 mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
 # Install flannel networking.
-kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/bc79dd1505b0c8681ece4de4c0d86c5cd2643275/Documentation/kube-flannel.yml
+# kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/bc79dd1505b0c8681ece4de4c0d86c5cd2643275/Documentation/kube-flannel.yml
 
+#Install Calico networking
+kubectl apply -f https://docs.projectcalico.org/v3.3/getting-started/kubernetes/installation/hosted/rbac-kdd.yaml
+kubectl apply -f https://docs.projectcalico.org/v3.3/getting-started/kubernetes/installation/hosted/kubernetes-datastore/calico-networking/1.7/calico.yaml
 exit
